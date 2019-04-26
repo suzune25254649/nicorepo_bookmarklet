93ced4b8ebbb8a76a66ae4490100f85d4e014504
suzune25254649 <38400438+suzune25254649@users.noreply.github.com>
2019-04-26 22:58:05 +0900

    feat: 取得リンクを押して投稿動画リストを表示した後の場面でも、取得リンクが残るように修正しました。

--- inline.js
+++ inline.js
@@ -8,24 +8,10 @@ var xhr = new XMLHttpRequest();
 function main(days)
 {
 	DAYS = days;
-	if (smartphone)
-	{
-		document.open();
-		document.write('<a href="https://www.nicovideo.jp/my/top">PCページ</a>で実行してください');
-		document.close();
-		return;
-	}
-	if ('www.nicovideo.jp' != location.hostname)
-	{
-		document.open();
-		document.write('<a href="https://www.nicovideo.jp/my/top">ニコニコ動画</a>を開いた状態で実行してください');
-		document.close();
-		return;
-	}
+	jsons = [];
+	cursor = ''
 
-	document.open();
-	document.write('loading:[]<br>');
-	document.close();
+	document.getElementById("movies").innerHTML = 'loading:[]<br>';
 
 	loader();
 }
@@ -40,24 +26,22 @@ function loader()
 			jsons.push(js);
 			cursor = "cursor=" + js.meta.minId + "&";
 
-			var data = js.data[js.data.length - 1];
-			var now = new Date(); 
-			var dt = new Date(); 
-			dt.setDate(now.getDate() - DAYS);
-			var tim = new Date(data.createdAt);
-
-			document.open();
-			document.write('loading:[' + Math.floor(100 * (now.getTime() - tim.getTime()) / (DAYS * 24 * 60 * 60 * 1000)) + '％]<br>');
-			document.close();
-
-			if (dt < tim)
+			if (0 < js.data.length)
 			{
-				loader();
+				var data = js.data[js.data.length - 1];
+				var now = new Date(); 
+				var dt = new Date(); 
+				dt.setDate(now.getDate() - DAYS);
+				var tim = new Date(data.createdAt);
+
+				document.getElementById("movies").innerHTML = 'loading:[' + Math.floor(100 * (now.getTime() - tim.getTime()) / (DAYS * 24 * 60 * 60 * 1000)) + '％]<br>';
+				if (tim <= dt)
+				{
+					output();
+					return;
+				}
 			}
-			else
-			{
-				output();
-			}
+			loader();
 			return;
 		}
 	};
@@ -69,17 +53,13 @@ function loader()
 	}
 	catch(e)
 	{
-		document.open();
-		document.write("通信に失敗しました。ブラウザ特有のバグのようです。<br>もう一度ブックマークレットを起動するとうまく行くかも。");
-		document.close();
+		document.getElementById("movies").innerHTML = "通信に失敗しました。ブラウザ特有のバグのようです。<br>もう一度ブックマークレットを起動するとうまく行くかも。";
 	}
 }
 
 function output()
 {
-	var output = '<!DOCTYPE html><html><head></head><body>';
-	output += '<div><a target="_blank" href="https://www.nicovideo.jp/user/78823020/video"><img src="https://tn.smilevideo.jp/smile?i=32892185" align="left">広告：鈴音のピンクなボイロ動画一覧はこちら</a></div><br clear="all">';
-	output += '<hr>最近の動画投稿<hr>';
+	var output = '';
 	for(var i = 0; i < jsons.length; i++)
 	{
 		var js = jsons[i];
@@ -98,8 +78,5 @@ function output()
 			}
 		}
 	}
-	output += '</body></html>';
-	document.open();
-	document.write(output);
-	document.close();
+	document.getElementById("movies").innerHTML = output;
 }
